9463b236d07c9b1ebcf12c1634197484b0ca3484
furkancvsg <furkancvsg2@gmail.com>
2026-05-12 17:33:45 +0300

    Initial commit: Proje yapısı ve gitignore eklendi

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// JSON formatında veri alıp göndermek için [cite: 21]
+app.use(express.json());
+
+// Frontend dosyalarını (Vanilla JS) dışarıya açmak için [cite: 12]
+app.use(express.static("public"));
+
+// Basit bir test rotası
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Futbol sistemi API hazır!" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor`);
+});
